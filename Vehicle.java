f7f5e5df4e32e241046518320fe3735ea1a378d7
Christy C <91734372+Christykarunya@users.noreply.github.com>
2021-10-18 09:31:11 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,26 @@
+package week3.day1;
+
+public class Vehicle {
+
+	public void driverVehicle() {
+		System.out.println("Driving the Vehicle");
+
+	}
+
+	public void accelerate() {
+		System.out.println("Applied Acceleration");
+	}
+
+	public void applyBrake() {
+		System.out.println("Apply the Brake");
+
+	}
+
+	public void fillFuel() {
+		System.out.println("Fill the fuel");
+	}
+
+	public void shiftGear() {
+		System.out.println("Manually Shift the Gear");
+	}
+}
